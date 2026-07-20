c9d944c8eb74a2959b93937361fa8c687343153d
Gabriel Francisco Sant Ana <gabrielfranciscosantana@MacBook-Air-de-Gabriel.local>
2026-07-20 15:37:33 -0300

    Alterando main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 from flask import Flask
-from .upload.upload_handler import app as upload_app
+from upload.upload_handler import app as upload_app
+import awsgi
 
 app = Flask(__name__)
 app.register_blueprint(upload_app)
+
+
+def handler(event, context):
+    return awsgi.response(app, event, context)
