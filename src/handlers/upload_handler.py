45f7cfc6ea6ee52317f69777b52c3034def544b9
Gabriel Francisco Sant Ana <gabrielfranciscosantana@MacBook-Air-de-Gabriel.local>
2026-07-20 18:53:01 -0300

    Add mais tratativa de entrada

--- src/handlers/upload_handler.py
+++ src/handlers/upload_handler.py
@@ -1,6 +1,7 @@
 import json
 import base64
 
+
 from validators.xml_validator import validate_xml
 
 from services.s3_service import save_xml
@@ -10,64 +11,124 @@ from services.nfe_service import extract_metadata
 from services.dynamodb_service import save_metadata
 
 
+
 def upload(event, context):
 
     try:
 
-        # ===============================
-        # 1 - Receber XML
-        # ===============================
 
-        body = event.get("body")
+        # ==================================
+        # DEBUG API GATEWAY
+        # ==================================
+
+        print("==============================")
+        print("EVENT RECEBIDO")
+        print(event)
+        print("==============================")
+
+
+        # ==================================
+        # 1 - Receber XML
+        # ==================================
+
+        body = event.get(
+            "body"
+        )
+
+
+        print("==============================")
+        print("BODY RECEBIDO")
+        print(body)
+        print("==============================")
 
 
         if not body:
+
             raise Exception(
                 "XML não informado"
             )
 
 
-        if event.get("isBase64Encoded"):
+
+        # ==================================
+        # 2 - Converter XML
+        # ==================================
+
+        if event.get(
+            "isBase64Encoded"
+        ):
+
+
+            print(
+                "Body em Base64"
+            )
+
 
             xml_content = base64.b64decode(
                 body
             )
 
+
         else:
 
-            xml_content = body.encode(
+
+            xml_content = body.strip().encode(
                 "utf-8"
             )
 
 
-        # ===============================
-        # 2 - Validar XML
-        # ===============================
+
+        print("==============================")
+        print("XML RECEBIDO")
+        print(xml_content[:300])
+        print("==============================")
+
+
+
+        # ==================================
+        # 3 - Validar XML
+        # ==================================
 
         validate_xml(
             xml_content
         )
 
 
-        # ===============================
-        # 3 - Extrair dados NF-e
-        # ===============================
+        print(
+            "XML VALIDADO"
+        )
+
+
+
+        # ==================================
+        # 4 - Extrair Metadata NF-e
+        # ==================================
 
         metadata = extract_metadata(
             xml_content
         )
 
 
-        if not metadata.get("chave_acesso"):
+        print("==============================")
+        print("METADATA")
+        print(metadata)
+        print("==============================")
+
+
+
+        if not metadata.get(
+            "chave_acesso"
+        ):
 
             raise Exception(
                 "Chave de acesso NF-e não encontrada"
             )
 
 
-        # ===============================
-        # 4 - Salvar XML no S3
-        # ===============================
+
+        # ==================================
+        # 5 - Salvar XML S3
+        # ==================================
 
         s3_info = save_xml(
             xml_content
@@ -85,26 +146,53 @@ def upload(event, context):
         })
 
 
-        # ===============================
-        # 5 - Salvar Metadata DynamoDB
-        # ===============================
+        print(
+            "XML SALVO S3"
+        )
+
+
+
+        # ==================================
+        # 6 - Salvar DynamoDB
+        # ==================================
 
         save_metadata(
             metadata
         )
 
 
-        # ===============================
-        # 6 - Retorno
-        # ===============================
+        print(
+            "METADATA SALVO DYNAMODB"
+        )
+
+
+
+        # ==================================
+        # 7 - Response
+        # ==================================
 
         return {
 
+
             "statusCode": 200,
 
+
             "headers": {
-                "Content-Type": "application/json"
+
+                "Content-Type":
+                    "application/json",
+
+                "Access-Control-Allow-Origin":
+                    "*",
+
+                "Access-Control-Allow-Headers":
+                    "Content-Type,Authorization",
+
+                "Access-Control-Allow-Methods":
+                    "OPTIONS,POST"
+
             },
+
 
             "body": json.dumps({
 
@@ -122,21 +210,32 @@ def upload(event, context):
         }
 
 
+
     except Exception as e:
 
 
-        print(
-            f"Erro processamento NF-e: {str(e)}"
-        )
+        print("==============================")
+        print("ERRO PROCESSAMENTO NF-e")
+        print(str(e))
+        print("==============================")
 
 
         return {
 
+
             "statusCode": 500,
 
+
             "headers": {
-                "Content-Type": "application/json"
+
+                "Content-Type":
+                    "application/json",
+
+                "Access-Control-Allow-Origin":
+                    "*"
+
             },
+
 
             "body": json.dumps({
 
