7d9cdbffdefea791c87a3d9d9ac1812e0ec05c4a
Gabriel Francisco Sant Ana <gabrielfranciscosantana@MacBook-Air-de-Gabriel.local>
2026-07-21 11:35:56 -0300

    Ajustando pra barra nf duplicada

--- src/handlers/upload_handler.py
+++ src/handlers/upload_handler.py
@@ -7,7 +7,7 @@ from services.s3_service import save_xml
 
 from services.nfe_service import extract_metadata
 
-from services.dynamodb_service import save_metadata
+from services.dynamodb_service import (save_metadata, get_metadata)
 
 from shared.multipart import extract_file
 
@@ -100,8 +100,29 @@ def upload(event, context):
             raise Exception(
                 "Chave de acesso NF-e não encontrada"
             )
+        
+        # ==================================
+        # 3.1 - Verificar NF-e existente
+        # ==================================
+        existing_nfe = get_metadata(
+             metadata["nfe_id"]
+        )
 
-
+        if existing_nfe:
+            return {
+                 "statusCode": 409,
+                 "headers": {
+                 "Content-Type":
+                 "application/json",
+                 "Access-Control-Allow-Origin":
+                 "*"
+            },
+                "body": json.dumps({
+                "error":"NF-e já cadastrada",
+                "message": "A NF-e informada já existe no sistema.",
+                "nfe_id": metadata["nfe_id"]
+        })
+            }    
 
         # ==================================
         # 4 - Salvar XML S3
--- src/services/dynamodb_service.py
+++ src/services/dynamodb_service.py
@@ -2,8 +2,11 @@ import boto3
 
 from decimal import Decimal
 from zoneinfo import ZoneInfo
-from config import Config
+from datetime import datetime
 
+from botocore.exceptions import ClientError
+
+from config import Config
 
 
 dynamodb = boto3.resource(
@@ -12,12 +15,14 @@ dynamodb = boto3.resource(
 )
 
 
-
 table = dynamodb.Table(
     "tb-ralplast-nfe-metadata"
 )
 
 
+# ===================================
+# Converter float para Decimal
+# ===================================
 
 def convert_decimal(value):
 
@@ -53,10 +58,38 @@ def convert_decimal(value):
     return value
 
 
+# ===================================
+# Buscar NF-e
+# ===================================
 
+def get_metadata(nfe_id):
+
+    if not nfe_id:
+
+        raise Exception(
+            "nfe_id = chave NF-e obrigatória"
+        )
+
+
+    response = table.get_item(
+
+        Key={
+            "nfe_id": nfe_id
+        }
+
+    )
+
+
+    return response.get(
+        "Item"
+    )
+
+
+# ===================================
+# Salvar Metadata
+# ===================================
 
 def save_metadata(metadata):
-
 
     if not metadata.get(
         "nfe_id"
@@ -68,27 +101,20 @@ def save_metadata(metadata):
 
 
     # ===================================
-    # Criar ID DynamoDB
-    # ===================================
-
-    # metadata["nfe_id"] = (
-    #     metadata["chave_acesso"]
-    # )
-
-
-
-    # ===================================
     # Timestamp
     # ===================================
 
-    from datetime import datetime
-
-
     metadata["created_at"] = (
-        datetime.now(ZoneInfo("America/Sao_Paulo"))
-        .isoformat()
-    )
 
+        datetime.now(
+            ZoneInfo(
+                "America/Sao_Paulo"
+            )
+        )
+
+        .isoformat()
+
+    )
 
 
     # ===================================
@@ -100,10 +126,43 @@ def save_metadata(metadata):
     )
 
 
+    try:
 
-    table.put_item(
-        Item=item
-    )
+        table.put_item(
+
+            Item=item,
+
+            ConditionExpression=
+                "attribute_not_exists(nfe_id)"
+
+        )
+
+
+    except ClientError as e:
+
+
+        if (
+
+            e.response[
+                "Error"
+            ][
+                "Code"
+            ]
+
+            ==
+
+            "ConditionalCheckFailedException"
+
+        ):
+
+            raise Exception(
+
+                "NF-e já cadastrada no sistema"
+
+            )
+
+
+        raise
 
 
     return True
